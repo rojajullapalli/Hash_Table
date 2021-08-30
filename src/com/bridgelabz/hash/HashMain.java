7a05936769ec6dafec28292ca197baf0b39545c1
rojajullapalli <rojajules@gmail.com>
2021-08-30 21:10:52 +0530

    ADDED: Frequency Of Words In Large Paragraph

--- src/com/bridgelabz/hash/HashMain.java
+++ src/com/bridgelabz/hash/HashMain.java
@@ -2,18 +2,18 @@ package com.bridgelabz.hash;
 
 public class HashMain {
     public static void main(String[] args) {
-        String sentence = "to be or not to be";
-        HashMap<String,Integer> hashMap = new HashMap<>();
-        String[] words = sentence.toLowerCase().split(" ");
-        for (String word : words){
-            Integer value = hashMap.get(word);
-            if(value == null)
+        String sentence = "Paranoids are not paranoid because they are paranoid but because " +
+                "they keep putting themselves deliberately into paranoid avoidable situations";
+        HashMap<String, Integer> linkedHashMap = new HashMap<>();
+        String[] sentenceToWords = sentence.toLowerCase().split(" ");
+        for (String word : sentenceToWords) {
+            Integer value = linkedHashMap.get(word);
+            if (value == null)
                 value = 1;
             else
                 value = value + 1;
-            hashMap.addVal(word , value);
+            linkedHashMap.add(word, value);
         }
-        System.out.println(hashMap);
-
+        System.out.println(linkedHashMap);
     }
 }
--- src/com/bridgelabz/hash/HashMap.java
+++ src/com/bridgelabz/hash/HashMap.java
@@ -1,29 +1,53 @@
 package com.bridgelabz.hash;
 
+import java.util.ArrayList;
+
 public class HashMap<K, V> {
-    NewLinkList<K> NewLinkList;
+    private final int numBuckets;
+    ArrayList<NewLinkList<K>> bucketArray;
 
     public HashMap() {
-        this.NewLinkList = new NewLinkList<>();
+        this.numBuckets =10;
+        this.bucketArray = new ArrayList<>(numBuckets);
+        for (int i = 0; i < numBuckets; i++){
+            this.bucketArray.add(null);
+        }
+    }
+
+    private int getBucketIndex(K key) {
+        int hashCode = Math.abs(key.hashCode());
+        int index = hashCode % numBuckets;
+        return index;
     }
 
     public V get(K key) {
-        MapNode<K, V> mapNode = (MapNode<K, V>) this.NewLinkList.search(key);
+        int index = this.getBucketIndex(key);
+        NewLinkList<K> linkedList = this.bucketArray.get(index);
+        if(linkedList == null) {
+            return null;
+        }
+        MapNode<K, V> mapNode = (MapNode<K, V>) linkedList.search(key);
         return (mapNode == null) ? null : mapNode.getValue();
     }
 
-    public void addVal(K key, V value) {
-        MapNode<K, V> mapNode = (MapNode<K, V>) this.NewLinkList.search(key);
+    public void add(K key, V value) {
+        int index = this.getBucketIndex (key);
+        NewLinkList<K> linkedList = this.bucketArray.get(index);
+        if(linkedList == null) {
+            linkedList = new NewLinkList<> ();
+            this.bucketArray.set(index, linkedList);
+        }
+        MapNode<K, V> mapNode = (MapNode<K,V>) linkedList.search(key);
         if (mapNode == null) {
             mapNode = new MapNode<>(key, value);
-            this.NewLinkList.append(mapNode);
+            linkedList.append(mapNode);
         } else {
             mapNode.setValue(value);
         }
     }
-
     @Override
-    public String toString(){
-        return "MyHashMapNodes{"+NewLinkList+'}';
+    public String toString() {
+        return "MyLinkedHashMap List" + bucketArray + '}';
     }
 }
+
